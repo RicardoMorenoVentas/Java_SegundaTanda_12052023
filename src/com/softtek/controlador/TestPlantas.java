021d071499d4dafafae9775adaed80bed12415c2
Ricardo Moreno-Ventas <ricardo.morenoventas@softtek.com>
2023-05-12 13:58:14 +0200

    Initial commit

---
+++ src/com/softtek/controlador/TestPlantas.java
@@ -0,0 +1,17 @@
+package com.softtek.controlador;
+
+import com.softtek.modelo.plantas.Cactus;
+import com.softtek.modelo.plantas.PlantaTropical;
+
+public class TestPlantas {
+    public static void main(String[] args) {
+        Cactus c1 = new Cactus("Pinchitos",100);
+        PlantaTropical p1 = new PlantaTropical("Hojas",100);
+        System.out.println(c1.toString());
+        System.out.println(p1.toString());
+        c1.regar(10,1);
+        p1.regar(10,1);
+        System.out.println(c1.toString());
+        System.out.println(p1.toString());
+    }
+}
